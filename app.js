081f3fb7d86c70017f6f3b97cae87258b660e067
EfeÖ <efeoztin07@gmail.com>
2023-10-12 20:42:40 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-import { readFileSync, writeFileSync } from 'fs';
+fetch('db.json')
+    .then(response => response.json())
+    .then(data => {
+        // JSON verilerini 'data' değişkenine aldık
+        console.log(data);
+    });
 
-// JSON dosyasını oku
-const jsonData = fs.readFileSync('db.json', 'utf-8');
-const db = JSON.parse(jsonData);
-
-function create() {
     const yeniVeri = {
-        b1: {
-            header: '123',
-            content: '123',
-            short_content: '123',
-            date: '123',
-        }
+        başlık: 'Yeni Başlık',
+        içerik: 'Yeni İçerik'
     };
-
-    db.b1 = yeniVeri;
-
-    // JSON nesnesini dizeye dönüştür
-    const yeniJsonData = JSON.stringify(db, null, 2); // null ve 2, JSON formatını düzenlemek için girintileme ve boşluk ayarlarıdır.
-
-    // Yeni veriyi JSON dosyasına yaz
-    fs.writeFileSync('db.json', yeniJsonData, 'utf-8');
-}
+    
+    data.b1 = yeniVeri;
+    
